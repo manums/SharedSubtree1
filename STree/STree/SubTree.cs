52903d5bd6ab261b0319b412456077f3eb5046da
Manu Shivakumar <manu.machilles@outlook.com>
2017-01-16 17:42:50 +0530

    initial shared subtree

---
+++ STree/STree/SubTree.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STree
+{
+    public class SubTree
+    {
+        public void PrintSubTree()
+        {
+            Console.WriteLine("Subtree here");
+        }
+    }
+}
